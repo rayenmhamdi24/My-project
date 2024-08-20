6dd59fd6899369141fef47cded99f58a94887fd3
rayenmhamdi24 <rayenm374@gmail.com>
2024-08-20 11:49:29 -0600

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,23 @@
+function makeProfil(name,certificates,email,facebook,instagram,tiktok,hobbies){
+    return{
+        name:name,
+        certificates:certificates,
+        email:email,
+        facebook:facebook,
+       tiktok:tiktok,
+        hobbies:hobbies
+       }
+    }
+    var profil=makeProfil(
+        "Rayen Mhamdi",
+        function certificates(name,description,image){
+            return
+        }
+
+    
+        "rayenm374@gmail.com",
+        "https://www.facebook.com/profile.php?id=100011786647410&mibextid=ZbWKwL",
+        "https://www.tiktok.com/@rayemhamdi2024?_t=8p1k3JTjytN&_r=1",
+    
+    )
+   
