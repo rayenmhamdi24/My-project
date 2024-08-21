f6ed225a24d545201d660237708575c5a239eb07
rayenmhamdi24 <rayenm374@gmail.com>
2024-08-21 09:01:50 -0600

    my first project

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-function makeProfil(name,certificates,email,facebook,instagram,tiktok,hobbies){
+function makeProfil(name,certificates,email,facebook,tiktok,hobbies){
     return{
         name:name,
         certificates:certificates,
@@ -8,16 +8,30 @@ function makeProfil(name,certificates,email,facebook,instagram,tiktok,hobbies){
         hobbies:hobbies
        }
     }
-    var profil=makeProfil(
-        "Rayen Mhamdi",
-        function certificates(name,description,image){
-            return
-        }
+     var profil=makeProfil(
+         "Rayen Mhamdi",
+       ["NLP" ,"ITL"  ,"Reiki"],
+       "mailto:rayenm374@gmail.com",
+      "https://www.facebook.com/profile.php?id=100011786647410&mibextid=ZbWKwL", 
+        "https://www.tiktok.com/@rayemhamdi2024?_t=8p1k3JTjytN&_r=1",
+        ["singing","watching movies"]
+     )
+
+
+$("body").append(
+ `<div>${profil.name}</div>
+  <div> ${profil.certificates} </div>
+  <div><a href=" ${profil.email} ">Email me </a></div>
+  <div><a href="${ profil.facebook}">Facebook </a></div>
+   
+  <div><a href=${ profil.tiktok}>TikTok</a></div>
+  <div>${profil.hobbies} </div>`)
+   
 
     
-        "rayenm374@gmail.com",
-        "https://www.facebook.com/profile.php?id=100011786647410&mibextid=ZbWKwL",
-        "https://www.tiktok.com/@rayemhamdi2024?_t=8p1k3JTjytN&_r=1",
     
-    )
+
+    
+       
+       
    
